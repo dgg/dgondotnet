40c90490f32a17f46cfac9d39b1dad96e5a95383
Daniel González García <danielgonnet@gmail.com>
2013-08-27 16:25:40 +0200

    ale to display things to add

---
+++ src/DynamicCollections.Web/Controllers/AddController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using DgonDotNet.Blog.Samples.DynamicCollections.Domain;
+using DgonDotNet.Blog.Samples.DynamicCollections.Models;
+
+namespace DgonDotNet.Blog.Samples.DynamicCollections.Controllers
+{
+	public class AddController : DiscardController<AddViewModel>
+	{
+		private readonly IThingRepository _repository;
+
+		public AddController(IThingRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public ActionResult Index()
+		{
+			IEnumerable<Thing> things = _repository.Find();
+			var model = new AddViewModel
+			{
+				Things = AddableThing.FromThings(things)
+			};
+			return View(model);
+		}
+
+		[HttpPost]
+		public override ActionResult Index(string save, string cancel, AddViewModel posted)
+		{
+			return View();
+		}
+	}
+}
---
+++ src/DynamicCollections.Web/Models/AddableThing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DgonDotNet.Blog.Samples.DynamicCollections.Domain;
+
+namespace DgonDotNet.Blog.Samples.DynamicCollections.Models
+{
+	public class AddableThing
+	{
+		public AddableThing() { }
+
+		public AddableThing(Thing from)
+		{
+			Number = from.Number;
+			Text = from.Text;
+			IsEven = (from.Number % 2) == 0;
+		}
+
+		public string Text { get; set; }
+		public int Number { get; set; }
+		public bool IsEven { get; private set; }
+
+
+		public static AddableThing[] FromThings(IEnumerable<Thing> from)
+		{
+			return from.Select((t, i) => new AddableThing(t)).ToArray();
+		}
+	}
+}
