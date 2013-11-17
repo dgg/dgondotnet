31a691ffb30bb60ef6f738635fc263a4021539af
Daniel González García <danielgonnet@gmail.com>
2013-11-17 13:27:43 +0100

    sample transformations and two tasks

--- LoveButSometimes.build.ps1
+++ LoveButSometimes.build.ps1
@@ -1,5 +1,19 @@
 task default
 
-task TransformConfig {
-    
+task TransformConfig_HappyPath {
+    $transformer = ".\tools\WebConfigTransformRunner.1.0.0.1\WebConfigTransformRunner.exe"
+    $config = ".\src\LoveButSometimes.config"
+    $transformation = ".\src\LoveButSometimes.QA.config"
+    $target = ".\deploy\LoveButSometimes.config"
+
+    exec { &$transformer $config $transformation $target }
+}
+
+task TransformConfig_CloudsAndThunders {
+    $transformer = ".\tools\WebConfigTransformRunner.1.0.0.1\WebConfigTransformRunner.exe"
+    $config = ".\src\LoveButSometimes.config"
+    $transformation = ".\src\LoveButSometimes.QAA.config"
+    $target = ".\deploy\LoveButSometimes.config"
+
+    exec { &$transformer $config $transformation $target }
 }
