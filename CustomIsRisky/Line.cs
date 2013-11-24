0db871dd6a49618a9ddda84f8fbe08047d5dab46
Daniel González García <danielgonnet@gmail.com>
2013-11-24 11:25:41 +0100

    Add Order and Line

---
+++ CustomIsRisky/Line.cs
@@ -0,0 +1,11 @@
+﻿using NMoneys;
+
+namespace CustomIsRisky
+{
+	public class Line
+	{
+		public string ProductId { get; set; }
+		public uint Quantity { get; set; }
+		public Money Price { get; set; }
+	}
+}
---
+++ CustomIsRisky/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomIsRisky
+{
+	public class Order
+	{
+		public int Id { get; set; }
+		public string HeaderProperty { get; set; }
+		public Line[] Lines { get; set; }
+	}
+}
