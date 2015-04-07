24a08f1f2415e00f94155460a413f368f7c9b13c
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-04-07 21:02:51 +0200

    ported FunWithExpressions

---
+++ src/FunWithExpressions/BuiltComparisonTester.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DgonDotNet.Blog.Samples.FunWithExpressions
+{
+	[TestFixture]
+	public class BuiltComparisonTester
+	{
+		private static readonly Comparison<int> _canonical = (x, y) => x.CompareTo(y);
+		private static readonly Comparison<int> _template = (x, y) => (x > y) ? 1 : ((x < y) ? -1 : 0);
+
+		[Test]
+		public void Canonical_Offers_BaselineBehavior()
+		{
+			Assert.That(_canonical(1, 2), Is.EqualTo(-1));
+			Assert.That(_canonical(2, 1), Is.EqualTo(1));
+			Assert.That(_canonical(1, 1), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Template_SameBehavior_AsCanonical()
+		{
+			Assert.That(_template(1, 2), Is.EqualTo(-1));
+			Assert.That(_template(2, 1), Is.EqualTo(1));
+			Assert.That(_template(1, 1), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Generated_SameBehavior_AsCanonical()
+		{
+			Comparison<int> generated = new ComparisonBuilder<int>().Comparison;
+			Assert.That(generated(1, 2), Is.EqualTo(-1));
+			Assert.That(generated(2, 1), Is.EqualTo(1));
+			Assert.That(generated(1, 1), Is.EqualTo(0));
+		}
+	}
+}
---
+++ src/FunWithExpressions/ComparisonBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace DgonDotNet.Blog.Samples.FunWithExpressions
+{
+	public class ComparisonBuilder<T>
+	{
+		public ComparisonBuilder()
+		{
+			Type t = typeof(T), @int = typeof(int);
+			ParameterExpression x = Expression.Parameter(t, "x"), y = Expression.Parameter(t, "y");
+
+			Expression<Comparison<T>> expr = Expression.Lambda<Comparison<T>>(
+				Expression.Condition(
+					Expression.GreaterThan(x, y),
+					Expression.Constant(1, @int),
+					Expression.Condition(
+						Expression.LessThan(x, y),
+						Expression.Constant(-1, @int),
+						Expression.Constant(0, @int)
+						)
+					),
+				x, y);
+			Comparison = expr.Compile();
+		}
+
+		public Comparison<T> Comparison { get; private set; }
+	}
+}
