153b01aada5ce8a08f085eead2a07325f2f4032a
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-04-07 20:57:32 +0200

    ported EnumPartyEveryday

---
+++ src/EnumPartyEveryday/NoZeroOddity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DgonDotNet.Blog.Samples.EnumPartyEveryday
+{
+	[TestFixture]
+	public class NoZeroOddity
+	{
+		public enum NonZero
+		{
+			One = 1,
+			Two = 2
+		}
+
+		[Test]
+		public void DefaultValue_NotDefined()
+		{
+			Assert.That(Enum.IsDefined(typeof(NonZero), default(NonZero)), Is.False);
+		}
+	}
+}
