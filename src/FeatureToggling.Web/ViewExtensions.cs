3e1ec9bbdccbe467e356b5c8fef8e62c263f60f9
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-07-07 15:38:37 +0200

    Foundation site

---
+++ src/FeatureToggling.Web/ViewExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nancy.ViewEngines.Razor;
+
+namespace FeatureToggling.Web
+{
+	public static class ViewExtensions
+	{
+		private static string TopNavigation(this HtmlHelpers<object> helper)
+		{
+			return helper.RenderContext.Context.ViewBag.TopNavigation;
+		}
+
+		public static string ActiveFor(this HtmlHelpers<dynamic> helper, string navigation)
+		{
+			return navigation.Equals(helper.TopNavigation(), StringComparison.OrdinalIgnoreCase) ?
+				"active" :
+				string.Empty;
+		}
+	}
+}
