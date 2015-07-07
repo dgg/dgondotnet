bba4bdf175dedce813c63dc65549b48c1db7aa46
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-07-07 14:20:51 +0200

    add packages to become a nancy razor project

---
+++ src/FeatureToggling.Web/Modules/Home.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace FeatureToggling.Web.Modules
+{
+	public class Home : NancyModule
+	{
+		public Home()
+		{
+			Get["/"] = _ =>
+			{
+				return View["Home", "hello"];
+			};
+		}	 
+	}
+}
