f622ab2a7fc84b9d499673bbee269e4c91f39038
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-02-14 11:04:17 +0100

    baseline with commands and options

---
+++ src/NotLastConsole/OptionsForSomething.cs
@@ -0,0 +1,8 @@
+﻿namespace DgonDotNet.Blog.Samples.NotLastConsole
+{
+	public class OptionsForSomething
+	{
+		public string Location { get; set; }
+		public int Times { get; set; }
+		public bool Awesome { get; set; }
+	}
---
+++ src/NotLastConsole/OptionsForSomethingElse.cs
@@ -0,0 +1,8 @@
+﻿namespace DgonDotNet.Blog.Samples.NotLastConsole
+{
+	public class OptionsForSomethingElse
+	{
+		public string[] Locations { get; set; }
+		public bool NotSoAwesome { get; set; }
+	}
+}
