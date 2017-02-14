eff47d82548d53fa80cc4c7b5be3ea066025f7e2
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-02-14 11:34:05 +0100

    add Something command

---
+++ src/NotLastConsole_GoCommando/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DgonDotNet.Blog.Samples.NotLastConsole_GoCommando
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
---
+++ src/NotLastConsole_GoCommando/Something.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DgonDotNet.Blog.Samples.NotLastConsole;
+using GoCommando;
+
+namespace DgonDotNet.Blog.Samples.NotLastConsole_GoCommando
+{
+	[Command("something")]
+	[Description("Do something")]
+	public class Something : ICommand
+	{
+		private readonly ADoerOfSomething _command;
+
+		public Something()
+		{
+			_command = new ADoerOfSomething(Console.Out);
+		}
+
+		[Parameter("location", "l")]
+		[Description("place where the something was done")]
+		public string Location { get; set; }
+
+		[Parameter("times", "t", optional: true, defaultValue: "-1")]
+		[Description("include if the something was awesome")]
+		public int Times { get; set; }
+
+		[Parameter("awesome", "a", optional: true)]
+		[Description("number of times something was done")]
+		public bool Awesome { get; set; }
+
+		public void Run()
+		{
+			var options = new OptionsForSomething
+			{
+				Location = Location,
+				Awesome = Awesome,
+				Times = Times
+			};
+
+			_command.Do(options);
+		}
+	}
+}
