e3098d290e5f007c1e3939f60097933929717959
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-02-24 11:03:17 +0100

    add default value

--- src/NotLastConsole_PowerArgs/Something.cs
+++ src/NotLastConsole_PowerArgs/Something.cs
@@ -6,7 +6,7 @@ namespace DgonDotNet.Blog.Samples.NotLastConsole_PowerArgs
 	{
 		[ArgRequired, ArgShortcut("l"), ArgDescription("place where the something was done")]
 		public string Location { get; set; }
-		[ArgShortcut("t"), ArgDescription("number of times something was done")]
+		[ArgShortcut("t"), ArgDescription("number of times something was done"), ArgDefaultValue(1)]
 		public int Times { get; set; }
 		[ArgShortcut("a"), ArgDescription("include if the something was awesome")]
 		public bool Awesome { get; set; }
