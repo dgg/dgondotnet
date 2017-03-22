0b0a279984daba94e3bf4c63a0d6efdfde11b72c
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-03-22 14:43:30 +0100

    add command line utils sample

---
+++ src/NotLastConsole_CommandLineUtils/Program.cs
@@ -0,0 +1,43 @@
+﻿using DgonDotNet.Blog.Samples.NotLastConsole;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace DgonDotNet.Blog.Samples.NotLastConsole_CommandLineUtils
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var app = new CommandLineApplication();
+			app.HelpOption("-h | -? | --help");
+			
+			app.Commands.Add(new Something(app.OptionHelp.Template));
+
+			CommandArgument locations = null;
+			CommandOption awesome = null;
+			app.Command("something-else", somethingElse =>
+			{
+				somethingElse.Description = "Do something else";
+				somethingElse.HelpOption(app.OptionHelp.Template);
+
+				locations = somethingElse.Argument(
+					"-l | --locations", 
+					"places where the something else was done",
+					multipleValues: true);
+				awesome = somethingElse.Option("-a | --awesome", "include if the something else was awesome",
+					CommandOptionType.NoValue);
+			}).OnExecute(() =>
+			{
+				var cmd = new ADoerOfSomethingElse(app.Out);
+				cmd.Do(new OptionsForSomethingElse
+				{
+					Locations = locations.Values.ToArray(),
+					NotSoAwesome = !awesome.HasValue()
+				});
+
+				return 0;
+			});
+			app.Execute(args);
+		}
+	}
+}
---
+++ src/NotLastConsole_CommandLineUtils/Something.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using DgonDotNet.Blog.Samples.NotLastConsole;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace DgonDotNet.Blog.Samples.NotLastConsole_CommandLineUtils
+{
+	public class Something : CommandLineApplication
+	{
+		public Something(string optionHelpTemplate)
+		{
+			Name = "something";
+			Description = "Do something";
+
+			_location = Option("-l | --location", "place where the something was done", CommandOptionType.SingleValue);
+			_times = Option("-t | --times", "number of times something was done", CommandOptionType.SingleValue);
+			_awesome = Option("-a | --awesome", "include if the something was awesome", CommandOptionType.NoValue);
+
+			HelpOption(optionHelpTemplate);
+			OnExecute((Func<int>) run);
+		}
+
+		private int run()
+		{
+			if (!_location.HasValue())
+			{
+				Error.WriteLine($"The argument '{_location.LongName}' is required.");
+				ShowHelp(Name);
+				return 1;
+			}
+
+			var cmd = new ADoerOfSomething(Out);
+			int times;
+			if (!int.TryParse(_times.Value(), NumberStyles.Integer, CultureInfo.InvariantCulture, out times))
+			{
+				times = 1;
+			}
+			OptionsForSomething options = new OptionsForSomething
+			{
+				Location = _location.Value(),
+				Awesome = _awesome.HasValue(),
+				Times = times
+			};
+			cmd.Do(options);
+			return 0;
+		}
+
+		private readonly CommandOption _location, _awesome, _times;
+	}
+}
