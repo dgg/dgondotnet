17eacd420f31d42ba8d8282b949285076354cb72
Daniel González García <danielgonnet@gmail.com>
2018-01-03 15:42:20 +0100

    incompatible change. Breaks when replaces binary

---
+++ src/Library/ChildNs/Operation.cs
@@ -0,0 +1,15 @@
+﻿namespace BreakingChangeAnatomy.Library.ChildNs
+{
+	public static class Operation
+	{
+		public static TheReturn TheMethod(this TheType o, TheArgument argument)
+		{
+			// fake doing something
+			var result = new TheReturn
+			{
+				Something = $"{o.TheProperty}{argument.Postfix}"
+			};
+			return result;
+		}
+	}
+}
--- src/Library/TheType.cs
+++ src/Library/TheType.cs
@@ -1,19 +1,7 @@
-﻿using BreakingChangeAnatomy.Library.ChildNs;
-
-namespace BreakingChangeAnatomy.Library
+﻿namespace BreakingChangeAnatomy.Library
 {
-    public class TheType
-    {
-        public decimal TheProperty { get; set; }
-
-        public TheReturn TheMethod(TheArgument argument)
-        {
-            // fake doing something
-            var result = new TheReturn
-            {
-                Something = $"{TheProperty}{argument.Postfix}"
-            };
-            return result;
-        }
-    }
+	public class TheType
+	{
+		public decimal TheProperty { get; set; }
+	}
 }
